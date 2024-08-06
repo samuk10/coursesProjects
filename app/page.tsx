7207f336dd4594042cf7b3ddbdaef847b25100e3
Samuel Sampaio <samuel.sampaio@conectrj.com.br>
2024-08-06 14:39:18 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 import { SearchIcon } from "lucide-react";
 import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar";
+
+// use cni-[component] to add imports and cnx-[component] to use.
 
 const Home = () => {
   return (
@@ -18,6 +23,7 @@ const Home = () => {
             <SearchIcon />
           </Button>
         </div>
+
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -26,6 +32,29 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
