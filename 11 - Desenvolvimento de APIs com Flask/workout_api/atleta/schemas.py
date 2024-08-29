e9309991bb55c2484e3a02052d763f3eef69bb67
Samuel Sampaio <samukasampaio@hotmail.com>
2024-08-29 00:24:50 -0300

    chore: add models & schemas

--- 11 - Desenvolvimento de APIs com Flask/workout_api/atleta/schemas.py
+++ 11 - Desenvolvimento de APIs com Flask/workout_api/atleta/schemas.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
-from pydantic import BaseModel, Field, PositiveFloat
+from pydantic import Field, PositiveFloat
+from workout_api.contrib.schemas import BaseSchema
 
 
-class Atleta(BaseModel):
+class Atleta(BaseSchema):
     nome: Annotated[
         str, Field(description="Nome do atleta", examples=["João"], max_length=50)
     ]
--- 11 - Desenvolvimento de APIs com Flask/workout_api/main.py
+++ 11 - Desenvolvimento de APIs com Flask/workout_api/main.py
@@ -2,11 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Workout API")
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="0.0.0.0", port=8002, log_level="info", reload=True)
-
 
 @app.get("/")
 async def root():
