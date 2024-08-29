68ad50ebf8278fd588bb89adda59f8e98be60bd8
Samuel Sampaio <samuel.sampaio@conectrj.com.br>
2024-08-29 16:00:47 -0300

    fix: models and schemas for Endereço to endereco

--- 10 - Primeira API com FastAPI Python Docker/workout_api/centro_treinamento/models.py
+++ 10 - Primeira API com FastAPI Python Docker/workout_api/centro_treinamento/models.py
@@ -9,6 +9,6 @@ class CentroTreinamentoModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    endereço: Mapped[str] = mapped_column(String(60), nullable=False)
+    endereco: Mapped[str] = mapped_column(String(60), nullable=False)
     proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
     atleta: Mapped["AtletaModel"] = relationship(back_populates="centro_treinamento")
--- 10 - Primeira API com FastAPI Python Docker/workout_api/centro_treinamento/schemas.py
+++ 10 - Primeira API com FastAPI Python Docker/workout_api/centro_treinamento/schemas.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[
         str,
         Field(
@@ -13,15 +13,15 @@ class CentroTreinamento(BaseSchema):
             max_length=20,
         ),
     ]
-    Endereço: Annotated[
+    endereco: Annotated[
         str,
         Field(
-            description="Endereço do Centro de Treinamento",
+            description="Endereco do Centro de Treinamento",
             examples=["Av. Paulista, 1000"],
             max_length=60,
         ),
     ]
-    Proprietario: Annotated[
+    proprietario: Annotated[
         str,
         Field(
             description="Proprietario do Centro de Treinamento",
@@ -29,3 +29,18 @@ class CentroTreinamento(BaseSchema):
             max_length=30,
         ),
     ]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[
+        str,
+        Field(
+            description="Nome do Centro de Treinamento",
+            examples=["CT King"],
+            max_length=20,
+        ),
+    ]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description="ID do Centro de Treinamento")]
